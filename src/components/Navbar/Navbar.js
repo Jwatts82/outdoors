806ec3621d281a90f6069f7e3a74ae3a3f6bd66d
Jwatts82 <wattscodes@gmail.com>
2022-01-22 15:06:31 -0800

    transparent nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,32 +6,35 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className='nav'>
-        <div className='container'>
-          <h1 className='logo'>
-            <a href='#'>Outdoors</a>
-          </h1>
-          <ul>
-            <li>
-              <a href='#' className='current'>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>About</a>
-            </li>
-            <li>
-              <a href='#'>Parks</a>
-            </li>
-            <li>
-              <a href='#'>Trails</a>
-            </li>
-            <li>
-              <a href='#'>Favorites</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <nav className='nav'>
+          <div className='container'>
+            <h1 className='logo'>
+              <a href='#'>Outdoors</a>
+            </h1>
+            <ul>
+              <li>
+                <a href='#' className='current'>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#'>Parks</a>
+              </li>
+              <li>
+                <a href='#'>Trails</a>
+              </li>
+              <li>
+                <a href='#'>Favorites</a>
+              </li>
+              <li>
+                <a href='#'>Log In</a>
+              </li>
+              <li>
+                <a href='#'>Sign Up</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
     );
   }
 }
