b685021f86c97637929771781b6c346928863444
Jwatts82 <wattscodes@gmail.com>
2022-01-22 15:08:07 -0800

    rename folder to match file

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import Welcome from '../components/Home/Welcome';
+import Welcome from '../components/Welcome/Welcome';
 import '../App.css';
 
 function Home() {
