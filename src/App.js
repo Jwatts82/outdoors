91a0d40fee96f1b8e81c6cac4c5bea0c2dc1443f
Jwatts82 <wattscodes@gmail.com>
2022-01-22 16:01:29 -0800

    add park routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
+import Parks from './components/Parks/Parks';
+// import Trails from './components/Trails/Trails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <div>
         {/* <h1>OUTDOORS APP</h1> */}
         <Routes>
           <Route exact path='/' element={<Welcome />} />
-          {/* <Route exact path='/parks' componeent={Parks}/> */}
-          {/* <Route path ='parks/:id' component={ParksState}/> */}
+          <Route exact path='/parks' element={<Parks/>}/>
+          {/* <Route path ='parks/:id' element={<Trails/>}/> */}
         </Routes>
       </div>
     </Router>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
+import { NavLink } from 'react-router-dom';
 
-
-class Navbar extends Component {
-
-  render() {
-    return (
-        <nav className='nav'>
-          <div className='container'>
-            <h1 className='logo'>
-              <a href='#'>Outdoors</a>
-            </h1>
-            <ul>
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <div className='container'>
+        <h1 className='logo'>
+          <a href='#'>Outdoors</a>
+        </h1>
+        <ul>
+          <NavLink to='/' className='a'>
+            Home
+          </NavLink>
+          <NavLink to='/Parks' className='a'>
+            Parks
+          </NavLink>
+          <NavLink to='/Trails' className='a'>
+            Trails
+          </NavLink>
+          <NavLink to='/Camping' className='a'>
+            Camping
+          </NavLink>
+        </ul>
+        {/* <ul>
               <li>
                 <a href='#' className='current'>
                   Home
@@ -32,11 +44,10 @@ class Navbar extends Component {
               <li>
                 <a href='#'>Sign Up</a>
               </li>
-            </ul>
-          </div>
-        </nav>
-    );
-  }
-}
+            </ul> */}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
